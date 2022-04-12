95cd0ad40bcf36f93a5e464a9f2f8b5c36427541
pranshu199 <77734820+pranshu199@users.noreply.github.com>
2022-04-12 19:10:43 -0400

    Portfolio website

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#1d2e47] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+          </div>
+          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p> Nice to meet you.<br/> Please take a look around.</p>
+            </div>
+            <div>
+              <p>Innovative Front End Developer with 2 years of experience building and 
+                  maintaining responsive websites. Proficient in ReactJS, JavaScript, 
+                  MaterialUI, NodeJS and Htmls, CSS plus modern libraries and frameworks. </p>  
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
+import Resume from '../assets/resume.pdf';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Pranshu Sharma.
+        </h1>
+        <h2 className='text-2xl sm:text-5xl font-bold text-[#8892b0]'>
+           Front End Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I’m a Innovative front-end developer specializing in building exceptional digital experiences. 
+          Strong Believer of mobile first development. 
+          Aspiring to become a Full Stack Developer.
+        </p>
+        <div className='flex flex-row'>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            <Link to='contact' smooth={true} duration={500}>Contact</Link>
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+          <a  href={Resume} download className='text-white group border-2 px-6 py-3 my-2 mx-5 flex items-center md:hidden hover:bg-pink-600 bg-[rgb(122,41,112)] '>
+            Resume <BsFillPersonLinesFill size={30} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
