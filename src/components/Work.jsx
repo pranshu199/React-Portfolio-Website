4df9123981de8c9755de2c20c62c07fd19235b99
pranshu199 <77734820+pranshu199@users.noreply.github.com>
2022-04-16 19:13:33 -0400

    Changed the hover effect in Work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -44,7 +44,7 @@ const Work = () => {
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                ReactJS Chat App
+              Portfolio Website
               </span>
               <div className='pt-8 text-center'>
                 {/* <a href='/'>
