ce3840df6ed4e1eb3868a8a5d4f4662140f23770
pranshu199 <77734820+pranshu199@users.noreply.github.com>
2025-06-18 13:45:08 +0530

    updated project section with new project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,41 +1,66 @@
-import React from 'react';
-import Messenger from '../assets/messenger.png';
-import ApiSearch from '../assets/ApiSearch.png';
-import Portfolio from '../assets/portfolio.png';
-import Jobify from '../assets/jobify.png';
-import RecipeFinder from '../assets/recipeFinder.png';
-import PrompAi from '../assets/prompAi.png';
-
+import React from "react";
+import Messenger from "../assets/messenger.png";
+import ResumeBooster from "../assets/ResumeBooster.png";
+import Portfolio from "../assets/portfolio.png";
+import Jobify from "../assets/jobify.png";
+import RecipeFinder from "../assets/recipeFinder.png";
+import PrompAi from "../assets/prompAi.png";
 
 const Work = () => {
   return (
-    <div name='work' className='w-full h-full md:h-screen text-gray-300 bg-[#1d2e47] '>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+    <div
+      name="work"
+      className="w-full h-full md:h-screen text-gray-300 bg-[#1d2e47] "
+    >
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
-          <p className='py-6'>// Check out some of my recent work</p>
+          <p className="py-6">// Check out some of my recent work</p>
         </div>
-        
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
-            style={{ backgroundImage: `url(${PrompAi})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            style={{ backgroundImage: `url(${ResumeBooster})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Prompts for AI
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Resume Booster AI
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://project-promp-ai.vercel.app/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a href="https://resume-booster-blue.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/pranshu199/project_prompAi'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a href="https://github.com/pranshu199/ResumeBooster">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${PrompAi})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Prompts for AI
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://project-promp-ai.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/pranshu199/project_prompAi">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -44,67 +69,45 @@ const Work = () => {
           </div>
           <div
             style={{ backgroundImage: `url(${Jobify})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 Job Tracker App
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://www.jobify.live/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a href="https://www.jobify.live/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/pranshu199/JobsApp/tree/main'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a href="https://github.com/pranshu199/JobsApp/tree/main">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
               </div>
             </div>
           </div>
-          <div
-            style={{ backgroundImage: `url(${ApiSearch})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                ReactJS Api App
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='http://pranshu199.github.io/React.js-Api-Application'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='https://github.com/pranshu199/React.js-Api-Application'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+
           <div
             style={{ backgroundImage: `url(${Portfolio})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-              Portfolio Website
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Portfolio Website
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://pranshu199.github.io/React-Portfolio-Website/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a href="https://pranshu199.github.io/React-Portfolio-Website/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/pranshu199/React-Portfolio-Website'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a href="https://github.com/pranshu199/React-Portfolio-Website">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -113,45 +116,45 @@ const Work = () => {
           </div>
           <div
             style={{ backgroundImage: `url(${Messenger})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 ReactJS Chat App
               </span>
-              <div className='pt-8 text-center'>
+              <div className="pt-8 text-center">
                 {/* <a href='/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a> */}
-                <a href='https://github.com/pranshu199/cb5970'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a href="https://github.com/pranshu199/cb5970">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
               </div>
             </div>
           </div>
-          
+
           <div
             style={{ backgroundImage: `url(${RecipeFinder})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 Recipe Finder
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://pranshu199.github.io/recipe-finder/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a href="https://pranshu199.github.io/recipe-finder/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/pranshu199/recipe-finder'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a href="https://github.com/pranshu199/recipe-finder">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
